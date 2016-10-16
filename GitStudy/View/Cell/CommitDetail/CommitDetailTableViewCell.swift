61840382539726f18d46a501aff2168e9ab30f25
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-10-16 19:10:34 +0900

    Create CommitDetailTableViewCell

---
+++ GitStudy/View/Cell/CommitDetail/CommitDetailTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CommitDetailTableViewCell.swift
+//  GitStudy
+//
+//  Created by ShinokiRyosei on 2016/10/16.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class CommitDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
