f4ce1e40e6bd544bb5373b4fae776843d7f7563f
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-10-16 19:05:35 +0900

    Create CommitDetailViewController

---
+++ GitStudy/View/Home/Detail/CommitDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CommitDetailViewController.swift
+//  GitStudy
+//
+//  Created by ShinokiRyosei on 2016/10/16.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class CommitDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
