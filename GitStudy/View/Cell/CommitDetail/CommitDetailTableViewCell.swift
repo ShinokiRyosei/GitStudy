081d07e33565288e252c49257009b28e4b5d3bc1
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-10-16 22:30:53 +0900

    [Fix] CommitDetailTableViewCell layout and method

--- GitStudy/View/Cell/CommitDetail/CommitDetailTableViewCell.swift
+++ GitStudy/View/Cell/CommitDetail/CommitDetailTableViewCell.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class CommitDetailTableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var commitImageView: UIImageView!
+    @IBOutlet private weak var dateLabel: UILabel!
+    @IBOutlet private weak var commitMessageLabel: UILabel!
+    
+    func applyModel(of model: Commit?) {
+        guard let commit: Commit = model else { return }
+        commitImageView.image = commit.image
+        dateLabel.text = commit.createdAt.formatDate()
+        commitMessageLabel.text = commit.message
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
