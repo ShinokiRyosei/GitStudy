07068b0f8ec5d3c16414badf3e1b0687fa923cb0
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-10-16 19:09:10 +0900

    Add tableView

--- GitStudy/View/Home/Detail/CommitDetailViewController.swift
+++ GitStudy/View/Home/Detail/CommitDetailViewController.swift
@@ -7,11 +7,38 @@
 //
 
 import UIKit
+import JEToolkit
 
 class CommitDetailViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.registerCellClass()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+}
+
+extension CommitDetailViewController: UITableViewDelegate {
+    
+}
+
+extension CommitDetailViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.de
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
 }
