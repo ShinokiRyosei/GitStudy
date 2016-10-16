f4b99f585bc44443612f28380cf5c15d8ac45762
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-10-16 22:50:32 +0900

    Create NavigationController

---
+++ GitStudy/View/NavigationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationViewController.swift
+//  GitStudy
+//
+//  Created by ShinokiRyosei on 2016/10/16.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationBar.barTintColor = UIColor.blackTheme
+        self.navigationBar.tintColor = UIColor.whiteTheme
+    }
+}
