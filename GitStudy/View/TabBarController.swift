06c4b6d4253540de9a08082999e622d77c13a071
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-10-16 22:50:45 +0900

    Create TabBarController

---
+++ GitStudy/View/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  GitStudy
+//
+//  Created by ShinokiRyosei on 2016/10/16.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
